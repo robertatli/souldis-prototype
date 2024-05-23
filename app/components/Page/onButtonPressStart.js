f508de1197614bed06d324db91ca030ae8e243ed
robertatli <robbster10@gmail.com>
2024-05-23 14:08:21 +0000

    Small bugfix, now the Initial Touch Feedback works properly
    I was using the wrong value for the playHapticfeedback function

--- app/components/Page/onButtonPressStart.js
+++ app/components/Page/onButtonPressStart.js
@@ -1,9 +1,9 @@
 
 
 export const onButtonPressStartHandler = async (component, playHapticSequence) => {
-    const hapticSequence = [{"value": component.startHaptic.hapticValue}] || [];
+    const hapticSequence = [{"value": component.startHaptic.selectedHaptic}] || [];
     console.log("H1: ", hapticSequence);
-    console.log("H1: ", component.startHaptic.hapticValue);
+    console.log("H1: ", component.startHaptic.selectedHaptic);
     console.log("H1: ", component);
 
     switch (component.type) {
